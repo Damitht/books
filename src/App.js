5c3435f82eaaa67a3d360392e9d0fe38ecdcb8ed
Damitht <tenna_koon@yahoo.com>
2024-05-11 15:13:55 +1000

    Add delete and edit book functionalities

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { useState } from "react";
+import BookCreate from "./components/BookCreate";
+import BookList from "./components/BookList";
 
 function App(){
 const [books, setBooks] = useState([]);
 
-    return <div>App</div>
+    const createBook = (title) =>{
+        const updatedBooks = [
+            ...books,
+            {id:Math.round(Math.random() * 9999), 
+                title:title}
+        ];
+        setBooks(updatedBooks);
+    };
+
+    const deleteBookById = (id) => {
+        const deletedBooks = books.filter ((book) => {
+            return book.id !== id;
+        });
+
+        setBooks(deletedBooks);
+    };
+
+    return (
+        <div className="app">
+            <BookList books={books} onDelete={deleteBookById}/>
+            <BookCreate onCreate={createBook}/>
+        </div>
+    );
 }
 
 export default App;
